eae3bb99de9cb5f39df4f98a7a92d7b2596ca26d
mikonex <mikonex@gmail.com>
2026-03-28 17:19:23 +0100

    LAB_2 poprawione kolejny raz

--- Python/djangotutorial/polls/admin.py
+++ Python/djangotutorial/polls/admin.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 
-
 from .models import Choice, Question
 
 
 class ChoiceInline(admin.TabularInline):
     model = Choice
+
     extra = 3
+
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
@@ -15,7 +16,10 @@ class QuestionAdmin(admin.ModelAdmin):
     ]
     inlines = [ChoiceInline]
     list_display = ("question_text", "pub_date", "was_published_recently")
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
 
 
 admin.site.register(Question, QuestionAdmin)
+
 admin.site.register(Choice)
